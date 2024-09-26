a5c10cc74965443c2c3477b720e03c63108462a4
sheip9 <sheip9@outlook.com>
2024-09-26 22:37:39 +0800

    feat: return path when 404

--- internal/handler/app/record.go
+++ internal/handler/app/record.go
@@ -10,7 +10,9 @@ func GetRecord(c *gin.Context) {
 	var record *entity.Record = nil
 	err := (*db).First(&record, "path = ?", path).Error
 	if err != nil {
-		c.HTML(404, "404.html", gin.H{})
+		c.HTML(404, "404.html", gin.H{
+			"path": path,
+		})
 	}
 	c.Redirect(302, record.Value)
 }
