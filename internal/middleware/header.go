b48636a2a8757c72b09e0ed655a7867ac6b7fdf1
sheip9 <sheip9@outlook.com>
2024-10-19 08:43:05 +0800

    feat: constant version & fmt files

--- internal/middleware/header.go
+++ internal/middleware/header.go
@@ -2,11 +2,12 @@ package middleware
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/sheip9/ninelink/internal/constant"
 )
 
 func WithHeaderDealing() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Header("X-Powered-By", "NineLink")
+		c.Header("X-Powered-By", "NineLink"+constant.Version)
 		c.Next()
 	}
 }
